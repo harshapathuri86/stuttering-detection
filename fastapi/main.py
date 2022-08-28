61b217afb93196cb95eed39bf3eeee19d9dc536e
harsha <harsha.pathuri@students.iiit.ac.in>
2022-08-28 22:49:24 +0530

    changed for multiple files submission

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, UploadFile, HTTPException, Response, File
+from typing import List
 import json
 from model.helper import *
 
 app = FastAPI(
     title="Stuttering detection API",
-    description="Detect stuttering in audio files",
-    version="0.1.0",
+    description="Detect stuttering in audio files", version="0.1.0",
 )
 
 
@@ -22,15 +22,20 @@ def home():
 
 @app.post("/")
 # only allow wav format file to be uploaded
-async def stutter(audio: UploadFile = File(...)):
-    if audio.content_type not in ["audio/wave", "audio/wav"]:
-        raise HTTPException(400, detail="Invalid document type")
+async def stutter(audios: List[UploadFile] = File(...)):
+    for audio in audios:
+        if audio.content_type not in ["audio/wave", "audio/wav"]:
+            raise HTTPException(400, detail="Invalid document type of file {}".format(audio.filename))
     try:
-        print(audio.filename)
-        print(audio.content_type)
-        output = run_model(model, audio.file)
-        output = json.dumps(output)
-        return Response(content=output, media_type="application/json")
+        result = []
+        for audio in audios:
+            # print(audio.filename)
+            print(audio.content_type)
+            output = run_model(model, audio.file)
+            result.append(list(output))
+            # result[audio.filename] = output
+        result = json.dumps(result)
+        return Response(content=result, media_type="application/json")
     except Exception as e:
         print(e)
         raise HTTPException(500, detail="{}".format(e))
